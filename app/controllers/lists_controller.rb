2b49536e13718059c7697d0622b1782d3cfe70fd
Gerardo Calderon <gerardoh@calderonlopez.com>
2024-04-18 10:30:42 -0500

    Created index and show pages

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,5 +1,9 @@
 class ListsController < ApplicationController
   def index
-    @lists = Lists.all
+    @lists = List.all
+  end
+
+  def show
+    @list = List.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   get "lists", to: "lists#index"
+  get "lists/:id", to: "lists#show", as: "show"
 end
