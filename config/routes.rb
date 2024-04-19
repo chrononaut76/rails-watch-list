ddcac951887082e8ecdfc600fffb00a4ac4dc317
Gerardo Calderon <gerardoh@calderonlopez.com>
2024-04-18 19:28:34 -0500

    Implemented destroy in BookmarkController

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   # root "posts#index"
 
   resources :lists, only: %i[ index show new create ] do
-    resources :bookmarks, only: %i[ new create destroy ]
+    resources :bookmarks, only: %i[ new create ]
   end
+  resources :bookmarks, only: [ :destroy ]
 end
