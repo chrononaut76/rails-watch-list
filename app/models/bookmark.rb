77b3a1ec6e00119b92d93691f02855e3882cdcba
Gerardo Calderon <gerardoh@calderonlopez.com>
2024-04-20 09:27:46 -0500

    Refactoring indentation for readability

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -3,5 +3,8 @@ class Bookmark < ApplicationRecord
   belongs_to :list
 
   validates :comment, length: { minimum: 6 }
-  validates :list_id, uniqueness: { scope: :movie_id, message: '[movie, list] pairings should be unique' }
+  validates :list_id, uniqueness: {
+    scope: :movie_id,
+    message: '[movie, list] pairings should be unique'
+  }
 end
